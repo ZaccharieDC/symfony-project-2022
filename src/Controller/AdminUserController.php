8fc87367bf9d77fe5c5ee3fd8d71cb5eb37c2e3c
ZacLaptop <zaccharie.dicristofaro@gmail.com>
2022-11-25 14:16:34 +0100

    Feat: Création des CRUDs

---
+++ src/Controller/AdminUserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AdminUser;
+use App\Form\AdminUserType;
+use App\Repository\AdminUserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/user')]
+class AdminUserController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_user_index', methods: ['GET'])]
+    public function index(AdminUserRepository $adminUserRepository): Response
+    {
+        return $this->render('admin_user/index.html.twig', [
+            'admin_users' => $adminUserRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, AdminUserRepository $adminUserRepository): Response
+    {
+        $adminUser = new AdminUser();
+        $form = $this->createForm(AdminUserType::class, $adminUser);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $adminUserRepository->save($adminUser, true);
+
+            return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_user/new.html.twig', [
+            'admin_user' => $adminUser,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_user_show', methods: ['GET'])]
+    public function show(AdminUser $adminUser): Response
+    {
+        return $this->render('admin_user/show.html.twig', [
+            'admin_user' => $adminUser,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, AdminUser $adminUser, AdminUserRepository $adminUserRepository): Response
+    {
+        $form = $this->createForm(AdminUserType::class, $adminUser);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $adminUserRepository->save($adminUser, true);
+
+            return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_user/edit.html.twig', [
+            'admin_user' => $adminUser,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_user_delete', methods: ['POST'])]
+    public function delete(Request $request, AdminUser $adminUser, AdminUserRepository $adminUserRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$adminUser->getId(), $request->request->get('_token'))) {
+            $adminUserRepository->remove($adminUser, true);
+        }
+
+        return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Controller/AdvertController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Advert;
+use App\Form\AdvertType;
+use App\Repository\AdvertRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/advert')]
+class AdvertController extends AbstractController
+{
+    #[Route('/', name: 'app_advert_index', methods: ['GET'])]
+    public function index(AdvertRepository $advertRepository): Response
+    {
+        return $this->render('advert/index.html.twig', [
+            'adverts' => $advertRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_advert_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, AdvertRepository $advertRepository): Response
+    {
+        $advert = new Advert();
+        $form = $this->createForm(AdvertType::class, $advert);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $advertRepository->save($advert, true);
+
+            return $this->redirectToRoute('app_advert_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('advert/new.html.twig', [
+            'advert' => $advert,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_advert_show', methods: ['GET'])]
+    public function show(Advert $advert): Response
+    {
+        return $this->render('advert/show.html.twig', [
+            'advert' => $advert,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_advert_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Advert $advert, AdvertRepository $advertRepository): Response
+    {
+        $form = $this->createForm(AdvertType::class, $advert);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $advertRepository->save($advert, true);
+
+            return $this->redirectToRoute('app_advert_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('advert/edit.html.twig', [
+            'advert' => $advert,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_advert_delete', methods: ['POST'])]
+    public function delete(Request $request, Advert $advert, AdvertRepository $advertRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$advert->getId(), $request->request->get('_token'))) {
+            $advertRepository->remove($advert, true);
+        }
+
+        return $this->redirectToRoute('app_advert_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
